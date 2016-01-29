1d1338a1264f6745f1b42dd98d1fea28fe0412cd
FajarPurnama <fajarpurnama@localhost.localdomain>
2016-01-29 20:40:35 +0900

    modified:   embedpagehtml/lang/en/block_embedpagehtml.php

--- embedpagehtml/lang/en/block_embedpagehtml.php
+++ embedpagehtml/lang/en/block_embedpagehtml.php
@@ -1,8 +1,8 @@
 <?php
 $string['pluginname'] = 'Embed Page HTML Block';
-$string['embedpage'] = 'Embed Page HTML';
-$string['embedpage:addinstance'] = 'Add an embed page HTML block';
-$string['embedpage:myaddinstance'] = 'Add a new embed page HTML block to the My Moodle page';
+$string['embedpagehtml'] = 'Embed Page HTML';
+$string['embedpagehtml:addinstance'] = 'Add an embed page HTML block';
+$string['embedpagehtml:myaddinstance'] = 'Add a new embed page HTML block to the My Moodle page';
 $string['blockstring'] = 'Add link to external site';
 $string['blocktitle'] = 'Add title';
 $string['labelallowhtml'] = 'Allow HTML';
